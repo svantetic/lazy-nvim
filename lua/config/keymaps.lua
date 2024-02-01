6b24a6cc0bb231496960bbd70e87745853e85507
svantetic <arkadiusm@gmail.com>
2024-02-01 20:55:00 +0100

    update keymaps
    - remove default shift J
    - add "gd" as jump to definition

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+map("n", "gd", "<cmd>Telescope lsp_definitions<cr>")
+map("n", "<s-j>", "<nop>")
+map("n", "J", "<nop>")
